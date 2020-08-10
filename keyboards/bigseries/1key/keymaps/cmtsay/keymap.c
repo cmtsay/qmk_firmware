eaf4712732272ba9eaa613f3e371fd1a352cf8dc
Chris Tsay <cmtsay@iMac-5k.localdomain>
2020-08-10 14:07:09 -0500

    Add triple push to reset for cmtsay bigseries/1key

--- keyboards/bigseries/1key/keymaps/cmtsay/keymap.c
+++ keyboards/bigseries/1key/keymaps/cmtsay/keymap.c
@@ -29,7 +29,12 @@ LAYOUT(
 
 // Tap Dance definitions
 void runRegress(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count >= 2) {
+    if (state->count >= 3) {
+        // Reset the keyboard if you tap the key more than three times
+        reset_keyboard();
+        reset_tap_dance(state);
+    }
+    else if (state->count == 2) {
         SEND_STRING("runRegress --auto --runall\n");
         reset_tap_dance(state);
     }
