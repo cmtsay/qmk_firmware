7701ede27e73b53618501fc186c3f970679af73c
Chris Tsay <cmtsay@iMac-5k.local>
2021-11-18 16:23:38 -0600

    add numbers to lower on qwerty row

--- keyboards/boardwalk/keymaps/cmtsay/keymap.c
+++ keyboards/boardwalk/keymaps/cmtsay/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_FN] = LAYOUT_ortho_hhkb(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, _______, KC_APP,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, RESET,
         RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_UP, KC_BSLS, _______,
                  _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
