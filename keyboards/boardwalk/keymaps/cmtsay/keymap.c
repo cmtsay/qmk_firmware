b9dda52a642110cc61e285de58ce9fcbda4ec97d
Chris Tsay <cmtsay@iMac-5k.local>
2021-11-18 15:48:28 -0600

    move up arrow to fn layer and add dedicated lower

--- keyboards/boardwalk/keymaps/cmtsay/keymap.c
+++ keyboards/boardwalk/keymaps/cmtsay/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH,
         KC_ESC, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                 KC_LCTL, KC_LALT, KC_LGUI, MO(_FN),          KC_SPC,  LT(_RAISE,KC_SPC),           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+                 KC_LCTL, KC_LALT, KC_LGUI, MO(_FN),          KC_SPC, KC_SPC,  MO(_RAISE)         , KC_LEFT , KC_DOWN,   KC_RGHT
     ),
 
     /* Function Layer
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, KC_APP,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______,
         RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, RESET,
-        RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, _______,
+        RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_UP, KC_BSLS, _______,
                  _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
     ),
 
